886293707cc831bf1c6e125bd55f4fa205aaf497
jleibowitz <30394511+jleibowitz@users.noreply.github.com>
2018-06-23 22:27:40 -0400

    reorganized and beginning exploratory analysis, completed introductory organization of financial script

---
+++ Historical Data/compiling_financials_w_prices.R
@@ -0,0 +1,25 @@
+library("openxlsx")
+library("zoo")
+setwd("/Users/jeffrey/desktop/github/financialanalysis/Historical Data")
+
+ABT<-read.csv("ABT.csv")
+ABT_financials<-read.xlsx("ABT_financials.xlsx",rowNames=T)
+colnames(ABT_financials)<-convertToDate(colnames(ABT_financials))
+keptFinancials<-c("Gross Profit","Operating Income","Income Tax Expense","Net Income","Net Income Common","Gross Margin")
+kept<-ABT_financials[keptFinancials,]
+kept<-t(kept)
+
+ABT$Date<-as.Date(ABT$Date,format="%m/%d/%Y")
+
+  
+  
+Date<-row.names(kept)
+kept<-cbind(Date,kept)
+kept<-data.frame(kept)
+kept$Date<-as.Date(kept$Date)
+
+
+
+data<-merge(ABT,kept,by.x="Date",by.y="Date", all.y=TRUE, all.x=TRUE)
+
+data<-data[-c(1:7074),]
