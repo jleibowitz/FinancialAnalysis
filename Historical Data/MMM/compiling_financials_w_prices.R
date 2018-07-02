eefdc8139758f9c48f4296075b670213aff14cca
jleibowitz <30394511+jleibowitz@users.noreply.github.com>
2018-07-02 08:12:08 -0400

    completed a few more stock tests, end sunday

---
+++ Historical Data/MMM/compiling_financials_w_prices.R
@@ -0,0 +1,27 @@
+library("openxlsx")
+library("zoo")
+setwd("/Users/jeffrey/desktop/github/financialanalysis/Historical Data/MMM")
+
+ticker<-read.csv("MMM.csv")
+financials<-read.xlsx("financials.xlsx",rowNames=T)
+colnames(financials)<-convertToDate(colnames(financials))
+keptFinancials<-c("Gross Profit","Operating Income","Income Tax Expense","Net Income","Net Income Common","Gross Margin")
+kept<-financials[keptFinancials,]
+kept<-t(kept)
+
+#ticker$Date<-as.Date(ticker$Date,format="%m/%d/%Y")
+ticker$Date<-as.Date(ticker$Date)
+  
+  
+Date<-row.names(kept)
+kept<-cbind(Date,kept)
+kept<-data.frame(kept)
+kept$Date<-as.Date(kept$Date)
+
+
+
+data<-merge(ticker,kept,by.x="Date",by.y="Date", all.y=TRUE, all.x=TRUE)
+
+
+data<-na.omit(data)
+write.xlsx(data,"MMM_financials_quarterlyinfo.xlsx")
